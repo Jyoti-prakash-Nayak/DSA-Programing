53516b8e8db06923d569043aea5b8d20b31655ae
Jyoti-prakash-Nayak <87771215+Jyoti-prakash-Nayak@users.noreply.github.com>
2026-09-06 06:52:17 +0530

    Leetcode-115 06/09/26

---
+++ 115. Distinct Subsequences.cpp
@@ -0,0 +1,40 @@
+// T.C=O(n*m)
+// S.C=O(n*m)+(n+m)
+class Solution
+{
+public:
+    int f(int ind1, int ind2, string &s, string &t, vector<vector<int>> &dp)
+    {
+        if (ind2 == 0)
+        {
+            return 1;
+        }
+        if (ind1 == 0)
+        {
+            return 0;
+        }
+
+        if (dp[ind1][ind2] != -1)
+        {
+            return dp[ind1][ind2];
+        }
+
+        if (s[ind1 - 1] == t[ind2 - 1])
+        {
+            return dp[ind1][ind2] = (f(ind1 - 1, ind2 - 1, s, t, dp) + f(ind1 - 1, ind2, s, t, dp));
+        }
+        else
+        {
+            return dp[ind1][ind2] = f(ind1 - 1, ind2, s, t, dp);
+        }
+    }
+
+    int numDistinct(string s, string t)
+    {
+        int n = s.size();
+        int m = t.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return f(n, m, s, t, dp);
+    }
+};
